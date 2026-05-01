7f73ea9e9e0c9a1dd5bbf6901b5826806cf57bc5
May2367 <mayankhanda140677@gmail.com>
2026-05-01 01:28:38 -0400

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            l = s[left]
+            r = s[right]
+
+            while left < right and not('a' <= l <= 'z' or 'A' <= l <= 'Z' or '0' <= l <= '9'):
+                left += 1
+                l = s[left]
+
+            while left < right and not('a' <= r <= 'z' or 'A' <= r <= 'Z' or '0' <= r <= '9'):
+                right -= 1
+                r = s[right]
+
+            if l != r:
+                print(l, r)
+                return False
+            else:
+                left += 1
+                right -= 1
+
+        return True
