eaa5940aca09ff86fa2767281d6c93b0aaebd631
May2367 <mayankhanda140677@gmail.com>
2026-05-03 15:38:23 -0400

    Add: counting-bits - submission-5

---
+++ Data Structures & Algorithms/counting-bits/submission-5.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        #Brute Force
+        output = [0] * (n + 1)
+        for i in range(n + 1):
+            curr_ttl = 0
+            for j in range(10):
+                curr_ttl += (i >> j) & 1
+            
+            output[i] = curr_ttl
+
+        return output
