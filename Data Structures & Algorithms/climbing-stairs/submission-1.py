d515357a4dfea66ac50669661d2f158ae7887067
May2367 <mayankhanda140677@gmail.com>
2026-05-05 19:25:56 -0400

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [1] * (n+1)
+
+        for i in range(2, len(dp)):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+
+        # Run Time: O(n), Space Complexity: O(n)
