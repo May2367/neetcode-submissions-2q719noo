862900b2509bc0a5e3c95916d4c9fa6666fbab1c
May2367 <mayankhanda140677@gmail.com>
2026-05-09 19:36:44 -0400

    Add: coin-change-ii - submission-7

---
+++ Data Structures & Algorithms/coin-change-ii/submission-7.py
@@ -0,0 +1,42 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for num in coins:
+            for i in range(1, len(dp)):
+                if i - num >= 0:
+                    dp[i] += dp[i - num]
+
+        return dp[amount]
+
+'''
+let amount = 4, coins = [1, 2]
+
+for i in range(1, len(dp)):
+    for num in coins:
+        i = 1, 2, 3, 4
+        coins = 1, 2
+        (1, 1), (2, 1), (2, 2), (3, 1), (3, 2), (4, 1), (4, 2)...
+        0: set with empty set
+        1: {{1}}
+        2: {{2}, {1, 1}}
+        3: {{1, 2}, {2, 1}, {1, 1, 1}} -- allows for duplicates
+
+for num in coins:
+    for i in range(1, len(dp)):
+        num = 1, 2
+        i = 1, 2, 3, 4
+        (num, i): (1, 1), (1, 2), (1, 3), (1, 4), (2, 2), (2, 3), (2, 4)
+        0: set with empty set
+        1: {{1}}
+        2: {{1, 1}, {2}}
+        3: {{1, 1, 1}, {1, 2}}
+        4: {{1, 1, 1, 1}, {1, 1, 2}, {2, 2}}
+
+Final Thoughts: the first pass adds sequences using only the val 1. Then
+the second pass allows the optional use of val 2. By iterating over vals
+first as opposed to indices, we can control how sequences are built.
+'''
+
+        # Run Time: O(n*k), Space Complexity: O(n)
